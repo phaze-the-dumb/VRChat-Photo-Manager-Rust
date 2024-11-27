f9f05e014e3b37c2b1f4f288962809a75e1f1ce8
phaze-the-dumb <phaze@duck.com>
2024-11-27 17:59:26 +0000

    hjklhjklhjklhjklhjkl

--- src-tauri/src/util/handle_uri_proto.rs
+++ src-tauri/src/util/handle_uri_proto.rs
@@ -27,6 +27,8 @@ pub fn handle_uri_proto( request: Request<Vec<u8>>, responder: UriSchemeResponde
     // becomes a potential issue if the frontend gets modified or there's an issue with tauri.
 
     let path = uri.path().split_at(1).1;
+    dbg!(path);
+    
     let file = fs::File::open(path);
 
     match file {
