2b2c9540c0693ee3ec2b8ae93b0bf2ffe57a817e
phaze-the-dumb <phaze@duck.com>
2024-11-27 17:49:10 +0000

    kjhljhklkhjjkhlhjklhjkkhjhkjhjkl

--- src-tauri/src/frontend_calls/relaunch.rs
+++ src-tauri/src/frontend_calls/relaunch.rs
@@ -2,13 +2,15 @@ use std::process::{ self, Command };
 
 #[tauri::command]
 pub fn relaunch() {
-  let container_folder = dirs::config_dir()
-    .unwrap()
-    .join("PhazeDev\\VRChatPhotoManager");
+  #[cfg(windows)]{
+    let container_folder = dirs::config_dir()
+      .unwrap()
+      .join("PhazeDev\\VRChatPhotoManager");
 
-  let mut cmd = Command::new(&container_folder.join("./vrchat-photo-manager.exe"));
-  cmd.current_dir(container_folder);
-  cmd.spawn().expect("Cannot run updater");
-
-  process::exit(0);
+    let mut cmd = Command::new(&container_folder.join("./vrchat-photo-manager.exe"));
+    cmd.current_dir(container_folder);
+    cmd.spawn().expect("Cannot run updater");
+  
+    process::exit(0);
+  }
 }
