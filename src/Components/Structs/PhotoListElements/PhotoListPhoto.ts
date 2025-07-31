d9bd90d25ea138122cd50d363839069fc027d95e
phaze-the-dumb <phaze@duck.com>
2025-07-31 19:20:54 +0100

    oops i meant to merge that

--- src/Components/Structs/PhotoListElements/PhotoListPhoto.ts
+++ src/Components/Structs/PhotoListElements/PhotoListPhoto.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Photo } from "../Photo";
 import { PhotoListElement } from "../PhotoListElement";
 import { PhotoListElementType } from "../PhotoListElementType";
@@ -11,18 +10,4 @@ export class PhotoListPhoto extends PhotoListElement{
     this.Type = PhotoListElementType.PHOTO;
     this.Photo = photo;
   }
-=======
-import { Photo } from "../Photo";
-import { PhotoListElement } from "../PhotoListElement";
-import { PhotoListElementType } from "../PhotoListElementType";
-
-export class PhotoListPhoto extends PhotoListElement{
-  public Photo!: Photo;
-
-  constructor( photo: Photo ){ 
-    super(); 
-    this.Type = PhotoListElementType.PHOTO;
-    this.Photo = photo;
-  }
->>>>>>> e612756 (stuff)
 }
