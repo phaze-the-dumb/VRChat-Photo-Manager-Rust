85d425616622ec1a153762cfbc61552739b2d234
phaze-the-dumb <phaze@duck.com>
2025-08-19 15:07:18 +0100

    Fix build script

--- build-release.sh
+++ build-release.sh
@@ -14,11 +14,11 @@ pnpm tauri build --debug --runner cargo-xwin --target x86_64-pc-windows-msvc
 mkdir builds
 mkdir builds/$VERSION
 
-mv src-tauri/target/release/bundle/appimage/vrchat-photo-manager_0.0.1_amd64.AppImage builds/$VERSION/vrcpm-$VERSION.AppImage
-mv src-tauri/target/release/bundle/deb/vrchat-photo-manager_0.0.1_amd64.deb builds/$VERSION/vrcpm-$VERSION.deb
-mv src-tauri/target/release/bundle/rpm/vrchat-photo-manager-0.0.1-1.x86_64.rpm builds/$VERSION/vrcpm-$VERSION.rpm
+mv "src-tauri/target/release/bundle/appimage/VRChat Photo Manager_0.0.1_amd64.AppImage" builds/$VERSION/vrcpm-$VERSION.AppImage
+mv "src-tauri/target/release/bundle/deb/VRChat Photo Manager_0.0.1_amd64.deb" builds/$VERSION/vrcpm-$VERSION.deb
+mv "src-tauri/target/release/bundle/rpm/VRChat Photo Manager-0.0.1-1.x86_64.rpm" builds/$VERSION/vrcpm-$VERSION.rpm
 
 mv src-tauri/target/x86_64-pc-windows-msvc/release/VRChatPhotoManager.exe builds/$VERSION/vrcpm-$VERSION.exe
 mv src-tauri/target/x86_64-pc-windows-msvc/debug/VRChatPhotoManager.exe builds/$VERSION/vrcpm-$VERSION-debug.exe
 
-mv src-tauri/target/x86_64-pc-windows-msvc/release/bundle/nsis/vrchat-photo-manager_0.0.1_x64-setup.exe builds/$VERSION/vrcpm-$VERSION-setup.exe
+mv "src-tauri/target/x86_64-pc-windows-msvc/release/bundle/nsis/VRChat Photo Manager_0.0.1_x64-setup.exe" builds/$VERSION/vrcpm-$VERSION-setup.exe
